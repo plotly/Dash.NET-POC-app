974ab079fe5cd0ec6eabd360568910e6d8bc555a
rpkyle <ryan@plotly.com>
2020-09-22 23:53:44 -0400

    :tada: initial commit

---
+++ src/Dash.NET/DashApp.fs
@@ -0,0 +1,104 @@
+﻿namespace Dash.NET
+
+open Giraffe
+open Views
+open Plotly.NET
+open System 
+open System.Collections.Generic
+open System.Runtime.InteropServices
+
+type DashApp =
+    {
+        Index: IndexView
+        Layout: obj //This will have a proper type when the DSL for components is in place
+        Config: DashConfig
+        Callbacks: CallbackMap
+        Dependencies: DashDependency list
+    }
+
+    static member initDefault() =
+        {
+            Index = IndexView.initDefault()
+            Layout = obj
+            Config = DashConfig.initDefault()
+            Callbacks = CallbackMap()
+            Dependencies = []
+
+        }
+
+    static member initDefaultWith (initializer: DashApp -> DashApp) = DashApp.initDefault () |> initializer
+
+    static member withConfig (config: DashConfig) (app: DashApp) = 
+        { app with 
+            Config = config 
+            Index = app.Index |> IndexView.withConfig config
+        }
+
+    static member withIndex (index:IndexView) (app:DashApp) =
+        { app with 
+            Index = index |> IndexView.withConfig app.Config
+        }
+
+    static member withLayout (layout:obj) (app:DashApp) =
+        { app with 
+            Layout = layout
+        }
+
+    static member withCallbackHandler (callbackId: string, callback: Callback<'Function>) (app: DashApp) =
+        
+        let dashDependency = Callback.toDashDependency callback
+
+        //To-Do: Maybe use copy utility for all direct calls to underlying DynamicObjs.
+        { app with
+            Callbacks =
+                app.Callbacks
+                |> CallbackMap.registerCallback callbackId callback
+
+            Dependencies = dashDependency::app.Dependencies
+        }
+
+    static member withCallbackHandlers (callbacks: seq<(string * Callback<'Function>)>) (app: DashApp) =
+        //To-Do: Maybe use copy utility for all direct calls to underlying DynamicObjs.
+        { app with
+            Callbacks =
+                callbacks
+                |> Seq.fold (fun cMap (cId, cHandler) ->
+                    cMap
+                    |> CallbackMap.registerCallback cId cHandler) app.Callbacks
+        }
+
+    static member getIndexHTML (app:DashApp) =
+        app.Index |> IndexView.toHTMLComponent
+
+    static member toWebApp (app:DashApp) =
+        choose [
+            GET >=>
+                choose [
+                    //serve the index
+                    route "/" >=> htmlView (app |> DashApp.getIndexHTML)
+
+                    //Dash GET enpoints
+                    route "/_dash-layout"       >=> json app.Layout        //Calls from Dash renderer for what components to render (must return serialized dash components)
+                    route "/_dash-dependencies" >=> json app.Dependencies  //Serves callback bindings as json on app start.
+                    route "/_reload-hash"       >=> json obj               //This call is done when using hot reload.
+                ]
+
+            POST >=> 
+                choose [
+                    //Dash POST endpoints
+                    route "/_dash-update-component" //calls from callbacks come in here.
+                        >=> bindJson ( fun (cbRequest:CallbackRequest) -> 
+
+                            let inputs = cbRequest.Inputs |> Array.map (fun reqInput -> box reqInput.Value) //generate argument list for the callback
+
+                            let result = 
+                                app.Callbacks
+                                |> CallbackMap.getPackedCallbackById (cbRequest.Output) //get the callback from then callback map
+                                |> Callback.getResponseObject inputs //evaluate the handler function and get the response to send to the client
+
+                            json result //return serialized result of the handler function
+                        )
+                    
+                ]
+            setStatusCode 404 >=> text "Not Found" 
+        ]
---
+++ src/Dash.NET/DashConfig.fs
@@ -0,0 +1,59 @@
+﻿namespace Dash.NET
+
+open Newtonsoft.Json
+open Plotly.NET
+
+type HotReloadSettings =
+    {
+        [<JsonProperty("intervall")>]
+        Intervall: int
+        [<JsonProperty("max_retry")>]
+        MaxRetry: int
+    }
+
+//todo: use standard casing and add serialization flags instead.
+type DashConfig =
+    {
+        [<JsonProperty("url_base_pathname")>]
+        UrlBasePathname: string Option
+        [<JsonProperty("requests_pathname_prefix")>]
+        RequestsPathamePrefix: string
+        [<JsonProperty("serve_locally")>]
+        ServeLocally: bool
+        [<JsonProperty("ui")>]
+        UI: bool
+        [<JsonProperty("props_check")>]
+        PropsCheck: bool
+        [<JsonProperty("show_undo_redo")>]
+        ShowUndoRedo: bool
+        [<JsonProperty("suppress_callback_exceptions")>]
+        SuppressCallbackExceptions: bool
+        [<JsonProperty("update_title")>]
+        UpdateTitle: string
+    }
+    static member create
+        urlBasePathname
+        requestsPathamePrefix
+        serveLocally
+        uI
+        propsCheck
+        showUndoRedo
+        suppressCallbackExceptions
+        updateTitle
+        =
+        {
+            UrlBasePathname = urlBasePathname
+            RequestsPathamePrefix = requestsPathamePrefix
+            ServeLocally = serveLocally
+            UI = uI
+            PropsCheck = propsCheck
+            ShowUndoRedo = showUndoRedo
+            SuppressCallbackExceptions = suppressCallbackExceptions
+            UpdateTitle = updateTitle
+        //hot_reload                  = {Intervall = 3000; MaxRetry = 8}
+        }
+
+    static member initDefault() =
+        DashConfig.create None "/" false true true false false "Updating..."
+
+    static member initDefaultWith(initializer: DashConfig -> DashConfig) = DashConfig.initDefault () |> initializer
